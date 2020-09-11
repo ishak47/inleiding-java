e06bf5099e9db9e6fd63bc0d34b7c0d0b1c299e8
ishak47 <ijmilou@roc-dev.com>
2020-09-11 13:53:13 +0200

    this is my first version. nothing changed yet

---
+++ src/h02/Een.java
@@ -0,0 +1,17 @@
+package h02;
+
+import java.applet.Applet;
+import java.awt.*;
+
+
+public class Een extends Applet {
+
+    public void init() {
+      setBackground(Color.blue);
+    }
+
+    public void paint(Graphics g) {
+     g.setColor(Color.yellow);
+     g.drawString("ishak",  950, 450 );
+    }
+}
---
+++ src/h02/Twee.java
@@ -0,0 +1,19 @@
+package h02;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class Twee extends Applet {
+
+    public void init() {
+        setBackground(Color.white);
+    }
+
+
+    public void paint(Graphics g) {
+        g.setColor(Color.blue);
+        g.drawString("ishak", 950, 430);
+            g.setColor(Color.red);
+            g.drawString("jmilou",950, 450);
+    }
+}
