6906b974f4fdb33943dcd72455a6ac4a7b4dccc9
ishak47 <ijmilou@roc-dev.com>
2020-09-17 11:52:28 +0200

    i finished "Opdracht2"

--- src/H06/Opdracht2.java
+++ src/H06/Opdracht2.java
@@ -4,19 +4,21 @@ import java.applet.Applet;
 import java.awt.*;
 
 public class Opdracht2 extends Applet {
-    int seconden,uur,dag,jaar,uitkomst;
+    int uur,dag,jaar,seconden,minuut;
 
     public void init() {
-    seconden=60;
-    uur=seconden*seconden;
-    dag=
-    jaar=52;
-    uitkomst=40;
+    seconden=1;
+    minuut=seconden*60;
+    uur=minuut*60;
+    dag=uur*24;
+    jaar=dag*365;
     }
 
 
     public void paint(Graphics g) {
-    g.drawString("seconden per uur is:"+ uur, 20,20);
-    g.drawString("seconden per dag is :"+ dag,20,35);
+    g.drawString("je hebt zoveel seconden per uur:"+uur,20,20);
+    g.drawString("je hebt zoveel seconden per dag:"+dag,20,40);
+    g.drawString("je hebt zoveel seconden per jaar:"+ jaar,20,60);
     }
+
 }
