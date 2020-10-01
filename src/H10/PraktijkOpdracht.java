8029cdaa8df42c9d3b1008fc5af2087a88b1cf28
ishak47 <ijmilou@roc-dev.com>
2020-10-01 12:27:24 +0200

    done with PraktijkOpdracht

--- src/H10/PraktijkOpdracht.java
+++ src/H10/PraktijkOpdracht.java
@@ -12,11 +12,11 @@ public class PraktijkOpdracht extends Applet {
     TextField tekstvak;
     Button ok;
     int getal;
-    int kleur;
+    String uitslag;
 
     public void init() {
+    //initialisatie
     getal=0;
-    kleur=0;
     tekstvak= new TextField("",20);
     ok=new Button("ok");
     ok.addActionListener(new CijferListener());
@@ -27,16 +27,40 @@ public class PraktijkOpdracht extends Applet {
     }
 
     public void paint(Graphics g) {
-
+        g.drawString("Jouw cijfer is: " + uitslag, 50, 50);
 
     }
-
 
     class CijferListener implements ActionListener{
 
         public void actionPerformed(ActionEvent e) {
             System.out.println("klik");
-
+        getal=Integer.parseInt(tekstvak.getText());
+        switch (getal){
+            case 1:
+            case 2:
+            case 3:
+                uitslag = "slecht";
+                break;
+            case 4:
+                uitslag= "onvoldoende";
+            break;
+            case 5:
+                uitslag= "matig";
+           break;
+            case 6:
+            case 7:
+                uitslag= "voldoende";
+            break;
+            case 8:
+            case 9:
+            case 10:
+                uitslag= "goed";
+            break;
+            default:
+                uitslag= "kan niet";
+        }
+        repaint();
         }
     }
 }
