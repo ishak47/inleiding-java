2939ed9d4094acbfc3c1c2cdf6340d8536443b42
ishak47 <ijmilou@roc-dev.com>
2020-10-02 14:11:24 +0200

    klaar met Opdracht3

---
+++ src/H11/Opdracht3.java
@@ -0,0 +1,23 @@
+package H11;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class Opdracht3 extends Applet {
+
+    public void init() {
+    }
+
+    public void paint(Graphics g) {
+        int Getal = 1;
+        int Getal2= 1;
+        int x = 10;
+
+        while (Getal < 100) {
+            x += 35;
+            g.drawString("," + Getal + "," + Getal2,x,20);
+            Getal += Getal2;
+            Getal2 += Getal;
+        }
+    }
+}
