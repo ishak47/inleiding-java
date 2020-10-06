781b9dedc53d9e4565796cdf2f3d1c4d9dd01927
ishak47 <ijmilou@roc-dev.com>
2020-10-06 11:26:14 +0200

    i started Opdracht9

---
+++ src/H11/Opdracht9.java
@@ -0,0 +1,15 @@
+package H11;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class Opdracht9 extends Applet {
+
+    public void init() {
+    }
+
+    public void paint(Graphics g) {
+        g.drawRect(50, 50, 200, 200);
+
+    }
+}
