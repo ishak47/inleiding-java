170c8566b9fdc8461556e9d76b95bae7020b0b30
ishak47 <ijmilou@roc-dev.com>
2020-10-06 12:25:02 +0200

    i am done with paint

--- src/H11/Praktijk1.java
+++ src/H11/Praktijk1.java
@@ -26,6 +26,19 @@ public class Praktijk1 extends Applet {
     }
 
     public void paint(Graphics g) {
+        for (int i = 1; i <= 10 ; i++);{
+            antwoord = a * i;
+            g.drawString("1 x " + a + "=",50,50);
+            g.drawString("2 x " + a + "=",50,70);
+            g.drawString("3 x " + a + "=",50,90);
+            g.drawString("4 x " + a + "=",50,110);
+            g.drawString("5 x " + a + "=",50,130);
+            g.drawString("6 x " + a + "=",50,150);
+            g.drawString("7 x " + a + "=",50,170);
+            g.drawString("8 x " + a + "=",50,190);
+            g.drawString("9 x " + a + "=",50,210);
+            g.drawString("10 x " +a + "=",45,230);
+        }
 
         }
 
