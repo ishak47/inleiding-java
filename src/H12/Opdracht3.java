eac9f264a14318f9503a3398ce7267cc87555568
ishak47 <ijmilou@roc-dev.com>
2020-10-08 11:38:24 +0200

    helemaal klaar met op3

--- src/H12/Opdracht3.java
+++ src/H12/Opdracht3.java
@@ -10,7 +10,7 @@ public class Opdracht3 extends Applet {
     //declaratie
     TextField input[] = new TextField[5];
     Button oke;
-    Button delete;
+
 
     int cijfer[] = new int[5];
 
@@ -24,14 +24,9 @@ public class Opdracht3 extends Applet {
         oke=new Button("oke");
         oke.addActionListener(new ButtonListener());
         add(oke);
-
-        delete=new Button("delete");
-        delete.addActionListener(new DeleteListener());
-        add(delete);
     }
 
     public void paint(Graphics g) {
-        delete.setLocation(340,40);
     }
 
     class ButtonListener implements ActionListener{
@@ -44,13 +39,6 @@ public class Opdracht3 extends Applet {
             for (int n = 0; n != 5; n++) {
                 input[n].setText(String.valueOf(cijfer[n]));
             }
-            repaint();
-        }
-    }
-
-    class DeleteListener implements ActionListener {
-        public void actionPerformed(ActionEvent e) {
-            input.setText("");
             repaint();
         }
     }
