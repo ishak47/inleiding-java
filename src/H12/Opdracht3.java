094b762fad636f0fa0b284b62dea4e72b9880ba3
ishak47 <ijmilou@roc-dev.com>
2020-10-08 11:00:47 +0200

    bijna klaar met opdracht3

--- src/H12/Opdracht3.java
+++ src/H12/Opdracht3.java
@@ -2,11 +2,15 @@ package H12;
 
 import java.applet.Applet;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Arrays;
 
 public class Opdracht3 extends Applet {
     //declaratie
     TextField input[] = new TextField[5];
     Button oke;
+    Button delete;
 
     int cijfer[] = new int[5];
 
@@ -16,6 +20,30 @@ public class Opdracht3 extends Applet {
         for (int i=0; i!=5; i++){
             input[i] = new TextField("",5);
             add(input[i]);
+        }
+        oke=new Button("oke");
+        oke.addActionListener(new ButtonListener());
+        add(oke);
+
+        delete=new Button("delete");
+        add(delete);
+        delete.setLocation(577,20);
+    }
+
+    public void paint(Graphics g) {
+    }
+
+    class ButtonListener implements ActionListener{
+
+        public void actionPerformed(ActionEvent e) {
+            for (int i = 0; i != 5; i++) {
+                cijfer[i] = Integer.parseInt(input[i].getText());
+            }
+            Arrays.sort(cijfer);
+            for (int i = 0; i != 5; i++) {
+                input[i].setText(String.valueOf(cijfer[i]));
+            }
+            repaint();
         }
     }
 }
