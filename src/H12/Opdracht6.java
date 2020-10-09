5d4bc4a744e265194766cfc2e602ef2e709136b1
ishak47 <ijmilou@roc-dev.com>
2020-10-09 09:54:38 +0200

    begonnen aan Opdracht6

---
+++ src/H12/Opdracht6.java
@@ -0,0 +1,17 @@
+package H12;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class Opdracht6 extends Applet {
+
+    //declaratie
+    int getallen[] = {7,3,5,1,8,2,9,10,3,2,6,1,7,8,5,2,3,10,2,1,8,1,4,6,3,7,9,10,2,1,5,8,2,4,8,3,4,7,1,5,3,2,6,7,10,9};
+    public void init() {
+
+    }
+
+    public void paint(Graphics g) {
+
+    }
+}
