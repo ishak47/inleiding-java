c098dc4bf8522340859fd50052850dbf610de98b
ishak47 <ijmilou@roc-dev.com>
2020-10-17 13:09:29 +0200

    klaar met opdracht1

--- src/H13/Opdracht1.java
+++ src/H13/Opdracht1.java
@@ -12,7 +12,12 @@ public class Opdracht1 extends Applet {
     }
 
     public void paint(Graphics g) {
-        TekenDriehoek(g, 50,50,75,25,100,50);
+        TekenDriehoek(g, 75,75,100,50,125,75);
     }
 
+    void TekenDriehoek(Graphics g, int x1, int y1, int x2, int y2, int x3, int y3){
+        g.drawLine(x1,y1,x2,y2);
+        g.drawLine(x2,y2,x3,y3);
+        g.drawLine(x3,y3,x1,y1);
+    }
 }
