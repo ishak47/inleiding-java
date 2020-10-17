b652c0a053c8b4abc5f197ea83d74ae0096c893f
ishak47 <ijmilou@roc-dev.com>
2020-10-17 13:07:52 +0200

    begonnen aan H13 opdracht1

---
+++ src/H13/Opdracht1.java
@@ -0,0 +1,18 @@
+package H13;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class Opdracht1 extends Applet {
+
+    // teken een driehoek m.b.v. de meegegeven data (parameters)
+
+    public void init() {
+
+    }
+
+    public void paint(Graphics g) {
+        TekenDriehoek(g, 50,50,75,25,100,50);
+    }
+
+}
