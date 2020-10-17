320f1a1f4fa5c11ca5b992b43c02b33f3e863af1
ishak47 <ijmilou@roc-dev.com>
2020-10-17 13:26:02 +0200

    begonnen aan opdracht 2. void StenenMuur aangemaakt

---
+++ src/H13/Opdracht2.java
@@ -0,0 +1,20 @@
+package H13;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class Opdracht2 extends Applet {
+
+    public void init() {
+    setSize(1000,500);
+    }
+
+    public void paint(Graphics g) {
+    StenenMuur (g,20,20,1000,500);
+    }
+
+    void  StenenMuur (Graphics g, int x, int y, int w, int h){
+
+    }
+
+}
