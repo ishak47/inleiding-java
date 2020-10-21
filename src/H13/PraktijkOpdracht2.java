b51e7f56e497905f6c0c9eef1ba1ba4a89fb1010
ishak47 <ijmilou@roc-dev.com>
2020-10-21 12:26:13 +0200

    klaar dankzij dylan

--- src/H13/PraktijkOpdracht2.java
+++ src/H13/PraktijkOpdracht2.java
@@ -2,7 +2,7 @@ package H13;
 
 import java.applet.Applet;
 import java.awt.*;
-import java.lang.reflect.GenericArrayType;
+
 
 public class PraktijkOpdracht2 extends Applet {
 
@@ -10,20 +10,26 @@ public class PraktijkOpdracht2 extends Applet {
     }
 
     public void paint(Graphics g) {
-    boomgaard(g,45,45,150,450);
+        boomgaard(g, 45, 45, 150, 450,5,2);
     }
 
-    void boomgaard (Graphics g, int x, int y, int w, int h) {
+    void boomgaard(Graphics g, int x, int y, int w, int h, int bomen , int rijen) {
 
- if (h < w * 1.5) {
-        h = w / 10 * 15;
+        for (int a = 0; a < rijen; a++) {
+            for (int b = 0; b < bomen; b++){
+
+                if (h < w * 1.5) {
+                    h = w / 10 * 15;
+                }
+
+            //de stam
+            g.setColor(new Color(110, 44, 31));
+            g.fillRect(x + (w / 5 * 2) + b * (w + 10), y + (w / 10 * 9) + a * (h + 20), w / 5 * 1, h - (w / 5 * 9));
+
+            //de bladeren
+            g.setColor(new Color(3, 66, 16));
+            g.fillOval(x + b * (w + 10 ) , y + a * ( h + 20 ) , w , w  );
+            }
+        }
     }
-    //de stam
-        g.setColor(new Color(110,44,31));
-        g.fillRect(x + (w / 5 * 2), y + (w / 5 * 2), w / 5, h - (w / 5 * 5));
-
-    //de bladeren
-        g.setColor(new Color(3, 66, 16));
-        g.fillOval(x,y,w,w);
-}
 }
