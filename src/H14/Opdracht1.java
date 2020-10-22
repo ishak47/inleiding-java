6f160d5022fc081372a8dfb19986165504103087
ishak47 <ijmilou@roc-dev.com>
2020-10-22 10:11:08 +0200

    action listener toegevoegd. werkt nog

---
+++ src/H14/Opdracht1.java
@@ -0,0 +1,40 @@
+package H14;
+
+import java.applet.Applet;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Opdracht1 extends Applet {
+
+    double cijferkeuze;
+    double kleurkeuze;
+
+    String[] nummer = {"aas", "twee", "drie", "vier", "vijf", "zes", "zeven", "acht", "negen", "tien", "Boer", "Vrouw", "Heer"};
+    String[] kleur = {"Ruiten", "Harten", "Schoppen", "Klaver"};
+    String keuze;
+    Button delen;
+
+
+    public void init() {
+        delen = new Button("deel kaart");
+        delen.addActionListener(new delen());
+        keuze = " uw gekozen kaart is :  ";
+        add(delen);
+
+    }
+
+    public void paint(Graphics g) {
+        g.drawString(keuze,20,50);
+    }
+
+    class delen implements ActionListener{
+
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    }
+
+
+
+}
