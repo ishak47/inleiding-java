c571690ec3fd925dcdb00a608e527b5983872256
ishak47 <ijmilou@roc-dev.com>
2020-10-22 11:25:07 +0200

    begonnen aan Opdracht2

---
+++ src/H14/Opdracht2.java
@@ -0,0 +1,18 @@
+package H14;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class Opdracht2 extends Applet {
+    String[] nummer = {"aas", "twee", "drie", "vier", "vijf", "zes", "zeven", "acht", "negen", "tien", "Boer", "Vrouw", "Heer"};
+    String[] kleur = {"Ruiten", "Harten", "Schoppen", "Klaver"};
+    String[] kaarten;
+    Button spelen;
+    public void init() {
+
+    }
+
+    public void paint(Graphics g) {
+
+    }
+}
