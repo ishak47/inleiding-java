726e299a7232b7ece7460c6c77e737c88b44d08c
ishak47 <ijmilou@roc-dev.com>
2020-10-27 10:45:14 +0100

    heb de ajax logo toegevoed

--- src/H14/PraktijkOpdracht.java
+++ src/H14/PraktijkOpdracht.java
@@ -3,14 +3,9 @@ package H14;
 import java.applet.Applet;
 import java.applet.AudioClip;
 import java.awt.*;
+import java.net.URL;
 
 public class PraktijkOpdracht extends Applet {
-
-    int ajax = 23;
-    boolean error = false;
-    boolean turn = false;
-    boolean lost = false;
-    boolean gameover = false;
 
     TextField tekstvak = new TextField("",5);
     Button speel = new Button("spelen");
@@ -19,16 +14,35 @@ public class PraktijkOpdracht extends Applet {
     String tekst1 = "voer 1, 2 of 3 in en druk op speel";
     String winnaar = "Winnaar!!";
 
-    public AudioClip sound;
-    private Image afbeelding;
+    int ajax = 23;
+    boolean error = false;
+    boolean turn = false;
+    boolean lost = false;
+    boolean gameover = false;
 
-
+    Image afbeelding;
 
     public void init() {
 
+    setSize(750,560);
+
+        //knoppen
+     //    speel.addActionListener(new speler());
+     //   opnieuw.addActionListener(new opnieuwListener());
+        add(tekstvak);
+        add(speel);
+        add(opnieuw);
+
+        //ajax logo
+        URL pad = PraktijkOpdracht.class.getResource("./resources/");
+        afbeelding= getImage(pad, "ajax logo H14.jpg");
     }
 
     public void paint(Graphics g) {
 
+        g.drawString(tekst,70,50);
+        g.drawString("er zijn nog: " + ajax  + " ajax logo's ",60,70);
     }
+
+
 }
